4b4216a5a06c044ac79979e758b6290ef39545ec
justinalin <justinalin@qnap.com>
2019-04-11 19:40:24 +0800

    refactor stack overflow client

--- pkg/service/stackoverflow_client.go
+++ pkg/service/stackoverflow_client.go
@@ -28,6 +28,8 @@ func (s *StackOverflowClient) Fetch(sort string, fromDate time.Time, page int) *
 	if page > MAXPAGE {
 		page = MAXPAGE
 	}
+
+REDIRECT:
 	resp, err := resty.R().
 		SetHeader("Content-Type", "x-www-form-urlencoded").
 		SetQueryParams(map[string]string{
@@ -42,10 +44,11 @@ func (s *StackOverflowClient) Fetch(sort string, fromDate time.Time, page int) *
 		}).
 		Get(url)
 
-	if resp.StatusCode() == 301 {
-		fmt.Println("error is ", resp.StatusCode(), " ", resp.Header(), " ", string(resp.Body()))
+	if resp.StatusCode() == 301 || resp.StatusCode() == 302 {
+		fmt.Println(resp.StatusCode(), " error happens ", string(resp.Body()))
+		goto REDIRECT
 	} else if resp.StatusCode() != 200 {
-		fmt.Println("error is ", resp.StatusCode(), " ,err is ", err, ", ", string(resp.Body()))
+		fmt.Println(resp.StatusCode(), " error happens ", err, ", ", string(resp.Body()))
 	}
 	result := &schema.Questions{}
 	err = json.Unmarshal(resp.Body(), result)
@@ -55,6 +58,7 @@ func (s *StackOverflowClient) Fetch(sort string, fromDate time.Time, page int) *
 
 func (s *StackOverflowClient) GetQuestion(url string) string {
 
+REDIRECT:
 	resp, err := resty.R().
 		SetHeader("accept-encoding", "gzip, deflate, br").
 		SetHeader("accept-language", "en-US,en;q=0.9").
@@ -64,15 +68,10 @@ func (s *StackOverflowClient) GetQuestion(url string) string {
 
 	if resp.StatusCode() == 301 || resp.StatusCode() == 302 {
 		url = resp.Header().Get("Location")
-		fmt.Println("301 error is ", resp.StatusCode(), " ", resp.Header().Get("Location"), " ", string(resp.Body()))
-		resp, err = resty.R().
-			SetHeader("accept-encoding", "gzip, deflate, br").
-			SetHeader("accept-language", "en-US,en;q=0.9").
-			SetHeader("cookie", "prov=5e56334e-146b-e335-0b16-d34f251d0000; _ga=GA1.2.1277608371.1544000152; __qca=P0-2078927393-1544000152440; __gads=ID=c2e064d30acd89e3:T=1544000153:S=ALNI_MYUoMVZ-DsUItNFOro-dKbCHlxm2A; notice-ctt=4%3B1544768195148; _gid=GA1.2.1761761962.1554869132; se-consent=%7b%22s%22%3a1%2c%22d%22%3a%222019-04-11T04%3a11%3a08.0872901Z%22%7d; _gat=1; hero-dismissed=1554955874709!stk_a").
-			SetHeader("User-Agent", "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36").
-			Get(url)
+		fmt.Println(resp.StatusCode(), " error happens ", string(resp.Body()))
+		goto REDIRECT
 	} else if resp.StatusCode() != 200 {
-		fmt.Println("!= 200 error is ", resp.StatusCode(), " ,err is ", err, ", ", string(resp.Body()))
+		fmt.Println(resp.StatusCode(), " error happens ", err, ", ", string(resp.Body()))
 	}
 
 	return string(resp.Body())
